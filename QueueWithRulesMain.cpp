9f8d950ed58081d75fa08cf0a979d73e61e6028e
Yten0chek <155365658+Yten0chek@users.noreply.github.com>
2025-06-11 18:44:05 +0500

    Queue_with_Rules

---
+++ QueueWithRulesMain.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+#include "Queue.h"
+
+bool stack(int a, int b) { return false; } //Работает как стек
+
+bool forMore(int a, int b) { return a < b; } //Сортировка по возрастанию
+
+bool forLess(int a, int b) { return a > b; } //Сортировка по убываниию
+
+int main()
+{
+    Queue<int> Al;
+
+    Al.SetRule(forMore);
+    
+    Al.push(1);
+    Al.push(4);
+    Al.push(2);
+    Al.push(3);
+    Al.push(8);
+
+    while (!Al.empty())
+        std::cout << Al.pop() << " ";
+}
+
